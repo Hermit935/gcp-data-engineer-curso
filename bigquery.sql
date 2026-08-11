66f6bc3df44612e12e2f22b3e37b18135c4fc5dd
Hiram Granados <xgranadosdhiramx@gmail.com>
2026-08-10 23:42:18 -0700

    curso-17

---
+++ bigquery.sql
@@ -0,0 +1,2 @@
+INSERT INTO mi_dataset_reportes.resumen_ventas VALUES ("CAMISETA", 1);
+SELECT * FROM mi_dataset_reportes.resumen_ventas;
--- setup-terraform/storege.tf
+++ setup-terraform/storege.tf
@@ -1,8 +1,8 @@
-resource "mi-bucket-unico-935" "auto-expire" {
+resource "google_storage_bucket" "auto_expire" {
     name                        = "mi-bucket-unico-935"
     location                    = "US"
     force_destroy               = true
-
+    uniform_bucket_level_access = true
 
     lifecycle_rule {
         condition {
